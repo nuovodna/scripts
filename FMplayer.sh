2c19bf8b99e0ce044740a732660d53b232a0062e
nuovodna <nuovodna@gmail.com>
2014-04-28 00:52:20 +0200

    Added RadioRai main stations

--- FMplayer.sh
+++ FMplayer.sh
@@ -1,2 +1,24 @@
-## Rai RadioTre
-mplayer -playlist http://www.listenlive.eu/rai3.m3u
+#!/bin/bash
+# Bash Menu Script Example
+
+PS3='Please enter your choice: '
+options=("Rai RadioUno" "Rai RadioDue" "Rai RadioTre" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+	"Rai RadioUno")
+        	mplayer -playlist http://www.listenlive.eu/rai1.m3u    	
+		;;
+        "Rai RadioDue")
+		mplayer -playlist http://www.listenlive.eu/rai2.m3u
+            	;;
+        "Rai RadioTre")
+	    	mplayer -playlist http://www.listenlive.eu/rai3.m3u
+            	;;
+        "Quit")
+            	break
+            	;;
+        *) echo invalid option;;
+    esac
+done
+
